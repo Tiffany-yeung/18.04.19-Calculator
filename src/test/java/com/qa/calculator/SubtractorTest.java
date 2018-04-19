18dbe4c7295d904b58d48f645292ff3de912a402
Tiffany Yeung <tiffany.yeung@qa.com>
2018-04-19 10:24:18 +0100

    "Subtractor test and class"

---
+++ src/test/java/com/qa/calculator/SubtractorTest.java
@@ -0,0 +1,21 @@
+package com.qa.calculator;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class SubtractorTest {
+
+	private Subtractor subtractor;
+	
+	@Test
+	public void test() {
+
+		subtractor = new Subtractor();
+		int expectedValue = 2;
+		int actualValue = subtractor.subtract(4, 2);
+		Assert.assertEquals(expectedValue, actualValue);
+		
+	}
+
+}
